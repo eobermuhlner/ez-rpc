2c91d778578e0b981edab056aa541dcaf2462cc2
Eric Obermühlner <eobermuhlner@gmail.com>
2016-11-25 17:44:31 +0100

    first version of annotations

---
+++ ch.obermuhlner.rpc/src/ch/obermuhlner/rpc/annotation/RpcField.java
@@ -0,0 +1,16 @@
+package ch.obermuhlner.rpc.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target({METHOD, FIELD})
+@Retention(RUNTIME)
+public @interface RpcField {
+
+	String name() default "";
+	
+}
---
+++ ch.obermuhlner.rpc/src/ch/obermuhlner/rpc/annotation/RpcMethod.java
@@ -0,0 +1,15 @@
+package ch.obermuhlner.rpc.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target({METHOD})
+@Retention(RUNTIME)
+public @interface RpcMethod {
+
+	String name() default "";
+	
+}
