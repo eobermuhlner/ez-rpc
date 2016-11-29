955119184248d679a5f3198ead220f5efa639c0b
Eric Obermühlner <eobermuhlner@gmail.com>
2016-11-29 07:50:21 +0100

    fix LocalTransportDirect to use MetaDataService

--- ch.obermuhlner.rpc.example/src/ch/obermuhlner/rpc/example/app/local/HelloServiceLocalTransportDirectApp.java
+++ ch.obermuhlner.rpc.example/src/ch/obermuhlner/rpc/example/app/local/HelloServiceLocalTransportDirectApp.java
@@ -4,6 +4,7 @@ import ch.obermuhlner.rpc.example.api.HelloService;
 import ch.obermuhlner.rpc.example.api.HelloServiceAsync;
 import ch.obermuhlner.rpc.example.client.HelloServiceClient;
 import ch.obermuhlner.rpc.example.server.HelloServiceImpl;
+import ch.obermuhlner.rpc.meta.MetaDataService;
 import ch.obermuhlner.rpc.service.ServiceFactory;
 import ch.obermuhlner.rpc.transport.LocalTransportDirect;
 
@@ -20,8 +21,9 @@ public class HelloServiceLocalTransportDirectApp {
 	
 		HelloServiceImpl helloServiceImpl = new HelloServiceImpl();
 		
-		LocalTransportDirect transport = new LocalTransportDirect();
-		ServiceFactory serviceFactory = new ServiceFactory(null);
+		MetaDataService metaDataService = new MetaDataService();
+		LocalTransportDirect transport = new LocalTransportDirect(metaDataService);
+		ServiceFactory serviceFactory = new ServiceFactory(metaDataService);
 		
 		serviceFactory.publishService(HelloService.class, helloServiceImpl, transport);
 		HelloService proxyService = serviceFactory.createRemoteService(HelloService.class, HelloServiceAsync.class, transport);
--- ch.obermuhlner.rpc/src/ch/obermuhlner/rpc/transport/LocalTransportDirect.java
+++ ch.obermuhlner.rpc/src/ch/obermuhlner/rpc/transport/LocalTransportDirect.java
@@ -2,13 +2,14 @@ package ch.obermuhlner.rpc.transport;
 
 import java.util.concurrent.CompletableFuture;
 
+import ch.obermuhlner.rpc.meta.MetaDataService;
 import ch.obermuhlner.rpc.service.Request;
 import ch.obermuhlner.rpc.service.Response;
 
 public class LocalTransportDirect extends ServerTransportImpl implements ClientTransport {
 
-	public LocalTransportDirect() {
-		super(null);
+	public LocalTransportDirect(MetaDataService metaDataService) {
+		super(metaDataService);
 	}
 	
 	@Override
