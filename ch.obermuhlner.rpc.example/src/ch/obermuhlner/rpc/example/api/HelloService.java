c6f26218dc5c9faa1bbfb23255259925f470722a
Eric Obermühlner <eobermuhlner@gmail.com>
2016-11-30 20:13:29 +0100

    add @RpcService to HelloService

--- ch.obermuhlner.rpc.example/src/ch/obermuhlner/rpc/example/api/HelloService.java
+++ ch.obermuhlner.rpc.example/src/ch/obermuhlner/rpc/example/api/HelloService.java
@@ -2,7 +2,9 @@ package ch.obermuhlner.rpc.example.api;
 
 import ch.obermuhlner.rpc.annotation.RpcMethod;
 import ch.obermuhlner.rpc.annotation.RpcParameter;
+import ch.obermuhlner.rpc.annotation.RpcService;
 
+@RpcService
 public interface HelloService {
 
 	void ping();
