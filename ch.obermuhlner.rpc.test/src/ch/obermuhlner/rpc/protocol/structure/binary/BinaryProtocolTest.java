c1046bb3050cac46388cfcdbe67e855178363dc8
Eric Obermühlner <eobermuhlner@gmail.com>
2016-11-30 21:56:35 +0100

    add BinaryProtocolTest

---
+++ ch.obermuhlner.rpc.test/src/ch/obermuhlner/rpc/protocol/structure/binary/BinaryProtocolTest.java
@@ -0,0 +1,14 @@
+package ch.obermuhlner.rpc.protocol.structure.binary;
+
+import ch.obermuhlner.rpc.meta.MetaDataService;
+import ch.obermuhlner.rpc.protocol.AbstractProtocolTest;
+import ch.obermuhlner.rpc.protocol.Protocol;
+
+public class BinaryProtocolTest extends AbstractProtocolTest {
+
+	@Override
+	protected <T> Protocol<T> getProtocol() {
+		MetaDataService metaDataService = new MetaDataService();
+		return new BinaryProtocol<>(metaDataService, getClass().getClassLoader());
+	}
+}
