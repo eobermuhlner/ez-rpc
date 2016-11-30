4f55064f226a70cd6fa9c3f933ba102972d45a70
Eric Obermühlner <eobermuhlner@gmail.com>
2016-11-30 07:44:07 +0100

    add HelloServiceImpl

---
+++ ch.obermuhlner.rpc.example.gen/src/ch/obermuhlner/rpc/example/server/HelloServiceImpl.java
@@ -0,0 +1,33 @@
+package ch.obermuhlner.rpc.example.server;
+
+import ch.obermuhlner.rpc.example.api.AdapterExampleData;
+import ch.obermuhlner.rpc.example.api.ExampleData;
+import ch.obermuhlner.rpc.example.api.HelloService;
+
+public class HelloServiceImpl implements HelloService {
+
+	@Override
+	public void ping() {
+		new RuntimeException("Ping").printStackTrace(System.out);
+	}
+
+	@Override
+	public Double calculateSquare(Double value) {
+		return value * value;
+	}
+
+	@Override
+	public ExampleData exampleMethod(ExampleData exampleData) {
+		return exampleData;
+	}
+
+	@Override
+	public AdapterExampleData adapterExampleMethod(AdapterExampleData adapterExampleData) {
+		return adapterExampleData;
+	}
+
+	@Override
+	public void exampleFailure() throws IllegalArgumentException {
+		throw new IllegalArgumentException("Example for an exception");
+	}
+}
