174353cd9127ead5f9f88dd0e86e99f5c32b0855
Eric Obermühlner <eobermuhlner@gmail.com>
2016-11-30 20:58:58 +0100

    add serializeToBytes() and deserializeFromBytes() to Protocol

--- ch.obermuhlner.rpc/src/ch/obermuhlner/rpc/protocol/Protocol.java
+++ ch.obermuhlner.rpc/src/ch/obermuhlner/rpc/protocol/Protocol.java
@@ -1,5 +1,7 @@
 package ch.obermuhlner.rpc.protocol;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -13,4 +15,19 @@ public interface Protocol<T> {
 	void serialize(OutputStream out, T element);
 	
 	T deserialize(InputStream in);
+	
+	default byte[] serializeToBytes(T element) {
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		serialize(byteArrayOutputStream, element);
+		return byteArrayOutputStream.toByteArray();
+	}
+
+	default T deserializeFromBytes(byte[] data) {
+		return deserializeFromBytes(data, 0, data.length);
+	}
+	
+	default T deserializeFromBytes(byte[] data, int offset, int length) {
+		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data, offset, length);
+		return deserialize(byteArrayInputStream);
+	}
 }
