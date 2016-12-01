f2db1b2aa98e06af49db20a637a1fa4f833a7a36
Eric Obermühlner <eobermuhlner@gmail.com>
2016-12-01 17:58:51 +0100

    add test case for exception throwing method

--- ch.obermuhlner.rpc.test/src/ch/obermuhlner/rpc/transport/AbstractTransportTest.java
+++ ch.obermuhlner.rpc.test/src/ch/obermuhlner/rpc/transport/AbstractTransportTest.java
@@ -1,6 +1,7 @@
 package ch.obermuhlner.rpc.transport;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -12,12 +13,14 @@ import ch.obermuhlner.rpc.annotation.RpcService;
 
 public abstract class AbstractTransportTest {
 
+	private static final String ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE = "This is an expected exception";
+
 	protected static TestService testService;
 
 	protected static TestServiceAsync testServiceAsync;
 
 	@Test
-	public void testService() {
+	public void testSync() {
 		testService.methodVoidToInt();
 		assertEquals(1, testService.methodVoidToInt());
 		assertEquals(2, testService.methodIntToInt(1));
@@ -32,11 +35,24 @@ public abstract class AbstractTransportTest {
 		assertEquals(Integer.valueOf(2), futureIntToInt.get());
 	}
 	
+	@Test
+	public void testThrowIllegalArgumentException() {
+		try {
+			testService.methodIllegalArgumentException();
+			fail("Expected an exception");
+		} catch(IllegalArgumentException e) {
+			assertEquals(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE, e.getMessage());
+		}
+	}
+	
 	@RpcService
 	public static interface TestService {
 		void methodVoidToVoid();
 		int methodVoidToInt();
 		int methodIntToInt(int value);
+		
+		int methodIllegalArgumentException();
+
 	}
 
 	public static interface TestServiceAsync {
@@ -57,6 +73,11 @@ public abstract class AbstractTransportTest {
 		@Override
 		public int methodIntToInt(int value) {
 			return 2;
+		}
+
+		@Override
+		public int methodIllegalArgumentException() {
+			throw new IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE);
 		}
 	}
 }
