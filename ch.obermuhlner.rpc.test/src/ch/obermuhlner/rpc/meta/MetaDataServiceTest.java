d4004334999dd1258fee37ca9627b9014fbf3860
Eric Obermühlner <eobermuhlner@gmail.com>
2016-12-12 18:01:51 +0100

    add MetaDataServiceTest

---
+++ ch.obermuhlner.rpc.test/src/ch/obermuhlner/rpc/meta/MetaDataServiceTest.java
@@ -0,0 +1,35 @@
+package ch.obermuhlner.rpc.meta;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import ch.obermuhlner.rpc.RpcException;
+
+public class MetaDataServiceTest {
+
+	@Test
+	public void testMetaDataLoadSame() {
+		try (MetaDataService metaDataService = new MetaDataService()) {
+			metaDataService.load(new File("testservice.xml"));
+			metaDataService.load(new File("testservice.xml"));
+		}
+		
+	}
+
+	@Test(expected = RpcException.class)
+	public void testMetaDataLoadDifferentServiceName() {
+		try (MetaDataService metaDataService = new MetaDataService()) {
+			metaDataService.load(new File("testservice.xml"));
+			metaDataService.load(new File("testservice_different_service_name.xml"));
+		}
+	}
+
+	@Test(expected = RpcException.class)
+	public void testMetaDataLoadDifferentServiceJavaName() {
+		try (MetaDataService metaDataService = new MetaDataService()) {
+			metaDataService.load(new File("testservice.xml"));
+			metaDataService.load(new File("testservice_different_service_javaname.xml"));
+		}
+	}
+}
